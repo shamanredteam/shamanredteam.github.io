984caf7ea35009f7dedfa7edbf32ec1caf98e97d
shaman0x01 <shaman0x01@localhost>
2023-10-07 11:59:07 +0400

    router updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route,Link } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import Home from "./pages";
 import ContactPage from "./pages/contact";
 
 function App() {
   return (
-    <Router>
-      <Routes>
+    <Routes>
         <Route path="/" element={<Home />} exact />
         <Route path="/contact" element={<ContactPage />} exact />
-      </Routes>
-    </Router>
+    </Routes>
   );
 }
 
